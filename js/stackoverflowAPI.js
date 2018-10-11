43918d8f20aa03f6c817e63c903b1d6e4a6de8b1
Eric Carlson <eric.paul.carlson@gmail.com>
2018-10-10 19:42:24 -0700

    updated stackoverflow api

--- js/stackoverflowAPI.js
+++ js/stackoverflowAPI.js
@@ -1,15 +1,29 @@
 function getDataFromStackOverflow(searchTerm, callback) {
     const query = {
-        search: searchTerm,
-        key: STACKOVERFLOWAPIKEY,
+        order: 'desc',
+        sort: 'votes',
+        site: 'stackoverflow',
+        intitle: searchTerm,
+        pagesize: 5
     }
     $.getJSON(STACKOVERFLOW_SEARCH_URL, query, callback)
+}
+
+function renderStackOverflowResults(result){
+    return `
+    <div>
+        <h3>
+        <a class='js-result-name' href=${result.link}' target='_blank'>${result.title}</a></h2>
+        </h3>
+    
+    </div>
+    `
 }
 
 function displayStackOverflowData(data) {
     console.log(data)
     searchData = data
 
-    const results = data.items.map((item, index) => renderResult(item));
+    const results = data.items.map((item, index) => renderStackOverflowResults(item));
     $('.js-stackoverflow').html(results);
 }
